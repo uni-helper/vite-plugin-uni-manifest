08a792ab7e11d8a3d17d33eedfc7e25661778aac
ModyQyW <wurui-dev@foxmail.com>
2022-11-15 14:25:26 +0800

    feat: update ManifestConfig types

--- src/config/types.ts
+++ src/config/types.ts
@@ -27,6 +27,40 @@ export interface NetworkTimeout {
   downloadFile?: number;
 }
 
+export interface UniStatistics {
+  /**
+   * 是否开启 uni 统计
+   * 默认为 false
+   */
+  enable: boolean;
+  /**
+   * uni 统计版本
+   * 默认为 1
+   */
+  version: "1" | "2";
+  /**
+   * 是否开启统计调试模式
+   * 生产阶段务必关闭
+   * 默认为 false
+   */
+  debug: boolean;
+  /**
+   * 前端数据上报周期
+   * 默认为 10
+   */
+  reportInterval: number;
+  /** 采集项配置 */
+  collectItems: {
+    /**
+     * 是否开启推送 PushClientID 的采集
+     * 默认为 false
+     */
+    uniPushClientID: boolean;
+  };
+}
+
+export interface SimpleUniStatistics extends Pick<UniStatistics, "enable"> {}
+
 export interface AppPlus {
   splashscreen?: any;
   screenOrientation?: any;
@@ -44,17 +78,42 @@ export interface AppPlus {
 }
 
 export interface ManifestConfig {
-  [x: string]: any;
+  /** 应用名称，安装 APP 后显示的名称 */
   name: string;
+  /**
+   * 应用标识，由 DCloud 云端分配
+   * 更多信息查看 <https://ask.dcloud.net.cn/article/35907>
+   */
   appid: string;
+  /** 应用描述 */
   description: string;
+  /**
+   * 当前默认语言
+   * 默认为 auto
+   */
   locale: string;
+  /** 版本名称，在云打包和生成 wgt 资源时使用 */
   versionName: string;
+  /** 版本号 */
   versionCode: string;
+  /**
+   * 是否转换 px 为 rpx
+   * 默认为 true
+   * 建议使用 false
+   */
   transformPx: boolean;
+  /** 网络超时时间 */
   networkTimeout: NetworkTimeout;
+  /**
+   * 是否开启 debug 模式
+   * 默认为 false
+   */
   debug: boolean;
-  uniStatistics: any;
+  /**
+   * uni 统计配置
+   * 更多信息查看 <https://uniapp.dcloud.net.cn/uni-stat-v1.html> 和 <https://uniapp.dcloud.net.cn/uni-stat-v2.html>
+   */
+  uniStatistics: UniStatistics;
   "app-plus": AppPlus;
   h5: any;
   quickapp: any;
