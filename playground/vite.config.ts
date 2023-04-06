f664e238cdc64ae30b88ec707da542739736ce9d
KeJunMao <kejun1997@gmail.com>
2023-04-06 15:43:58 +0800

    chore: update deps

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,5 +3,5 @@ import Uni from "@dcloudio/vite-plugin-uni";
 import UniManifest from "@uni-helper/vite-plugin-uni-manifest";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Uni(), UniManifest()],
+  plugins: [UniManifest(), Uni()],
 });
