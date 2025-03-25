f8432068bdbab80eb2841b21635e54f69639e9f6
Tom Gao <tom@tomgao.cc>
2025-03-25 19:33:33 +0800

    feat: add insertFinalNewline to UserOptions

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -4,6 +4,11 @@ export interface Options {
    * @default false
    */
   minify: boolean
+  /**
+   * insert newline at the end of the `manifest.json`
+   * @default false
+   */
+  insertFinalNewline: boolean
 }
 
 export interface UserOptions extends Partial<Options> {}
