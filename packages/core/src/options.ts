6251784f09aac51bb3b0151eeef0ed17a3bf66eb
Tom Gao <tom@tomgao.cc>
2025-10-13 10:51:54 +0800

    feat: add insertFinalNewline to UserOptions (#56)

--- packages/core/src/options.ts
+++ packages/core/src/options.ts
@@ -3,6 +3,7 @@ import type { ResolvedOptions, UserOptions } from './types'
 export function resolveOptions(userOptions: UserOptions): ResolvedOptions {
   return {
     minify: false,
+    insertFinalNewline: false,
     ...userOptions,
   }
 }
